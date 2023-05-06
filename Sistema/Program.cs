a7b46e014c0970333f4da269ea771c79f81d3d27
Samuel Zaire <96780102+szaire@users.noreply.github.com>
2023-05-06 17:17:18 -0300

    add new objective

--- Sistema/Program.cs
+++ Sistema/Program.cs
@@ -2,8 +2,9 @@
 {
     private static void Main(string[] args)
     {
-        // TODO: Entender a proposta do desafio
-        // TODO: Modelar o fluxograma do sistema
+        // TODO: Entender a proposta do desafio (ok)
+        // TODO: Modelar o Diagrama de Classes (ok)
+        // TODO: Modelar o fluxograma do sistema ()
         // TODO: Criar os pseudocódigos dos métodos
         // TODO: Implementar em C#
         Console.WriteLine("Hello, World!");
