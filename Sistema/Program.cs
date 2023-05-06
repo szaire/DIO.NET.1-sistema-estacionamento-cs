3b26bde57514d7bbf770d3470f732015a1bc018f
Samuel Zaire <96780102+szaire@users.noreply.github.com>
2023-05-06 08:56:57 -0300

    add Sistema files

---
+++ Sistema/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // TODO: Entender a proposta do desafio
+        // TODO: Modelar o fluxograma do sistema
+        // TODO: Criar os pseudocódigos dos métodos
+        // TODO: Implementar em C#
+        Console.WriteLine("Hello, World!");
+    }
+}
